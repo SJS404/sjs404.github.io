40bb306f97aacd8850e4d3f9b520cbea1b2b652a
SJS404 <s.schofield88@googlemail.com>
2024-09-26 10:02:55 +0100

    js

--- js/lbe.js
+++ js/lbe.js
@@ -11,9 +11,11 @@ function decodeAndInsert(linkString, className) {
 window.onload = function() {
     const links = {
         eLink: 'aHR0cHM6Ly90aW55dXJsLmNvbS81Y3M4cmF3Yw==',
-        tLink: 'aHR0cHM6Ly90aW55dXJsLmNvbS80ajVmYjk5Mgo='
+        tLink: 'aHR0cHM6Ly90aW55dXJsLmNvbS80ajVmYjk5Mgo=',
+        wLink: 'aHR0cHM6Ly90aW55dXJsLmNvbS8zdjU3azdwYg=='
     };
 
     decodeAndInsert(links.eLink, 'eLink');
     decodeAndInsert(links.tLink, 'tLink');
+    decodeAndInsert(links.wLink, 'wLink');
 };
